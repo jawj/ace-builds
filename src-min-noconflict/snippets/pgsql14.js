9f599eefa3c4f8789e89da662ab1112af872b9d9
Eduard Dyckman <bird.duskpoet@gmail.com>
2022-11-02 16:00:21 +0200

    Added pg14 and pg15 modes

---
+++ src-min-noconflict/snippets/pgsql14.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/pgsql14"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
