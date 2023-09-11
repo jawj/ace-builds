c00e69d21317e3da51bd0fcf1cbbf543156b6c66
Eduard Dyckman <bird.duskpoet@gmail.com>
2023-09-11 18:34:09 +0300

    Pg16 support

---
+++ src/snippets/pgsql16.js
@@ -0,0 +1,9 @@
+
+;                (function() {
+                    window.require(["ace/snippets/pgsql16"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
